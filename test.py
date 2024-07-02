2fd7fc484b57d0d299fcf7a534863b89b0b68721
Robson Vecchi <115637431+RobsonVecchi@users.noreply.github.com>
2024-07-02 08:52:28 -0300

    test.py

--- test.py
+++ test.py
@@ -2,6 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.patch(BASE + "video/1", json={"likes":12})
-print(response.json())
+
+"""response = requests.get(BASE + "video/2")
+print(response.json())"""
+
+"""response = requests.put(BASE + "video/3", json={"id":3, "name":"Third video", "views":600, "likes":500})
+print(response.json())"""
+
+"""response = requests.patch(BASE + "video/3", json={"likes":15})
+print(response.json())"""
+
+
+"""response = requests.delete(BASE + "video/3")
+print(f"Status Code: {response.status_code}")"""
 
